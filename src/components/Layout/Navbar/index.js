75d98a5a4d97606da227f6189bb1a933b73be075
Gustavoo1122 <gustavope883@gmail.com>
2026-06-03 16:46:36 -0300

    feat: make navbar responsive

--- src/components/Layout/Navbar/index.js
+++ src/components/Layout/Navbar/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import styles from './Navbar.module.css';
 
+import { IoMenu  } from "react-icons/io5";
+
 export default function Navbar() {
+
+  const [menu, setMenu] = useState(false);
+
   return (
-    <nav className={styles.navBar}>
-      <h2 className={styles.navTitle}>Seller Hub</h2>
-      <ul className={styles.navList}>
-          <li><Link to="/" className={styles.itemList}>Home</Link></li>
-          <li><Link to="/estoque" className={styles.itemList}>Estoque</Link></li>
-          <li><Link to="/financeiro" className={styles.itemList}>Financeiro</Link></li>
-      </ul>
-    </nav>
+    <header className={styles.header}>
+      <nav className={styles.navBar}>
+        <h2 className={styles.navTitle}>Seller Hub</h2>
+        <button
+          className={menu ? `${styles.menuButton} ${styles.menuButtonOpen}` : styles.menuButton}
+          onClick={() => setMenu(!menu)}
+        >
+          <IoMenu />
+        </button>
+        <ul className={menu ? `${styles.navList} ${styles.navListOpen}` : styles.navList} >
+            <li><Link to="/" className={styles.itemList} onClick={() => setMenu(false)}>Home</Link></li>
+            <li><Link to="/estoque" className={styles.itemList} onClick={() => setMenu(false)}>Estoque</Link></li>
+            <li><Link to="/financeiro" className={styles.itemList} onClick={() => setMenu(false)}>Financeiro</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
