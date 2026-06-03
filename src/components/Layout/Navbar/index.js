c936dd712dbdf10066ee8761a395a8fef09a139f
Gustavoo1122 <gustavope883@gmail.com>
2026-06-03 02:37:33 -0300

    feat: create initial header component

--- src/components/Layout/Navbar/index.js
+++ src/components/Layout/Navbar/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/estoque">Estoque</Link></li>
-        <li><Link to="/financeiro">Financeiro</Link></li>
-    </ul>
+    <nav className={styles.navBar}>
+      <h2 className={styles.navTitle}>Seller Hub</h2>
+      <ul className={styles.navList}>
+          <li><Link to="/" className={styles.itemList}>Home</Link></li>
+          <li><Link to="/estoque" className={styles.itemList}>Estoque</Link></li>
+          <li><Link to="/financeiro" className={styles.itemList}>Financeiro</Link></li>
+      </ul>
+    </nav>
   )
 }
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,20 +1,25 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import Navbar from "../components/Layout/Navbar";
 import Home from "../components/pages/Home";
 import Estoque from "../components/pages/Estoque";
 import Financeiro from "../components/pages/Financeiro";
+
+import Navbar from "../components/Layout/Navbar";
+import Container from "../components/Layout/Container";
+import Footer from "../components/Layout/Footer";
 
 export default function AppRoutes() {
   return (
     <Router>
         <Navbar />
-
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/estoque" element={<Estoque />} />
-            <Route path="/financeiro" element={<Financeiro />} />
-        </Routes>
+        <Container customClass="min-height">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/estoque" element={<Estoque />} />
+                <Route path="/financeiro" element={<Financeiro />} />
+            </Routes>
+        </Container>
+        <Footer />
     </Router>
   )
 }
